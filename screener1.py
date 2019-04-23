17ce3ef92b62a7309c5d08826cadfcd08fa2db1d
wd2lannoms <dlannom@me.com>
2019-04-23 16:57:28 -0500

    got some problems, run and see them

--- screener1.py
+++ screener1.py
@@ -1,21 +1,21 @@
-## this the first script used to extract and view text inside a pdf
-## this script extracts the requested number of pages and put it in a file
+## this the first script used to extract and view text inside a pdf ##
+## this script extracts the requested number of pages and puts it in a file ##
 from pdf2image import convert_from_path
 import pytesseract
 
-## specify the first and last pages
+## specify the first and last pages and computes the total number of pages ##
 first_page = input("What is the starting page?   ")
 first_page = int(first_page)
 last_page = input("What is the last page?   ")
 last_page = int(last_page)
+num_pages = last_page - first_page
 
-print("***** starting, standby..... *****")
-
+print("***** Starting the extraction, standby..... *****")
 ## set the target file variable name 4/21/19 ##
 ## Mike's Comment ##
 target_file = "/Users/dave/files/report.pdf"
 
-def pdf2txt(target_file, num_pages=100):
+def pdf2txt(target_file, num_pages):
     ##use pdf2image to keep the pages in memory
     pages = convert_from_path(target_file, 500, first_page, last_page)
     docs = []
@@ -27,7 +27,7 @@ def pdf2txt(target_file, num_pages=100):
         d['text'] = text 
         ##create list of dictionaries
         docs.append(d)
-    print("***** Done *****")
+    print(" ***** Extraction is complete ***** ")
     return docs
 text = pdf2txt(target_file,10)
 print(text)
